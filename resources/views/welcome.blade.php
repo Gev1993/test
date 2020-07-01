916f88be58edce7b0ed55ee33c74fff55c93cc5f
Gev 1993 <gevavetikyan39@gmail.com>
2020-07-01 20:23:10 +0400

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,6 +94,7 @@
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                     <a href="https://facebook.com">Facebook</a>
+                    <a href="https://instagram.com">Instagram</a>
                 </div>
             </div>
         </div>
